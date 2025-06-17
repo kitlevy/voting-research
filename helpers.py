86ba765af6e7ecd471055d761cc5470d1c650dfd
Kit Levy <levyec@bc.edu>
2025-06-17 14:33:17 -0400

    recent updates

--- helpers.py
+++ helpers.py
@@ -1,21 +1,13 @@
 import numpy as np
 from collections import Counter
 
-def generate_utilities(n):
-    #sampling uniformly from the 2D simplex (a, b, c utilities sum to 1)
-    a = np.random.rand(n)
-    b = np.random.rand(n)
-    c = 1 - a - b
-    mask = (c > 0)
-    return np.stack((a[mask], b[mask], c[mask]), axis=1)
-
-def test_generate_utilities(n):
+def generate_utilities_uniform(n):
     #rejection sampling from the 2D simplex
     samples = []
     while len(samples) < n:
-        a, b = np.random.rand(2)
-        c = 1 - a - b
-        if c >= 0:
+        a, c = np.random.rand(2)
+        b = 1 - a - c
+        if b >= 0:
             samples.append([a, b, c])
     return np.array(samples)
 
@@ -29,5 +21,33 @@ def count_profiles(rankings):
     labels = ['a', 'b', 'c']
     return Counter(''.join(labels[i] for i in rank) for rank in rankings)
 
-def a_over_c_fraction(u):
-    return np.mean(u[:, 0] > u[:, 2])
+def pairwise_fraction(u, i, j):
+    return np.mean(u[:, i] > u[:, j])
+
+def p_acb(u, alt1=0, alt2=2, alt3=1):
+    return np.mean((u[:, alt1] > u[:, alt2]) & (u[:, alt2] > u[:, alt3]))
+
+def get_prob(u, ranking):
+    if len(ranking) == 1:
+        i = ranking[0]
+        return np.mean((u[:, i] > u[:, (i + 1) % 3]) & (u[:, i] > u[:, (i + 2) % 3]))
+    elif len(ranking) == 2:
+        i, j = ranking
+        return np.mean(u[:, i] > u[:, j])
+    elif len(ranking) == 3:
+        i, j, k = ranking
+        return np.mean((u[:, i] > u[:, j]) & (u[:, j] > u[:, k]))
+    return []
+
+
+
+
+'''
+def wrong_generate_utilities(n):
+    #sampling uniformly from the 2D simplex (a, b, c utilities sum to 1)
+    a = np.random.rand(n)
+    b = np.random.rand(n)
+    c = 1 - a - b
+    mask = (c > 0)
+    return np.stack((a[mask], b[mask], c[mask]), axis=1)
+'''
--- plots.py
+++ plots.py
@@ -3,56 +3,120 @@ import matplotlib.pyplot as plt
 from helpers import *
 
 def simplex_to_cartesian(w):
-    #converting (w_a, w_b, w_c) to 2D Cartesian for plotting in an equilateral triangle
-    a, b, c = w
-    x = 0.5 * (2*b + c)  # or 0.5 * (b + 2*c)
-    y = (np.sqrt(3)/2) * c
-    return x, y
+    #a, b, c = w
+    cart_x = w[1] * 0.5 + w[2]
+    cart_y = w[1] * np.sqrt(3) / 2
 
-def plot_simplex_with_anchor(w_list, labels=None):
+    return cart_x, cart_y
+
+def apply_anchor(u, w, alpha):
+    return (1 - alpha) * u + alpha * w
+
+def plot_simplex_with_anchor(w_list, w_labels=None, alt_labels=None):
     corners = np.array([
-        [0.5, np.sqrt(3)/2],  # a
-        [0.0, 0.0],           # b
-        [1.0, 0.0]            # c
+        [0.0, 0.0],           #a
+        [0.5, np.sqrt(3)/2],  #b
+        [1.0, 0.0]            #c
     ])
-
+    middles = np.array([
+        [0.75, np.sqrt(3) / 4],  #op a
+        [0.5, 0.0],              #op b
+        [0.25, np.sqrt(3) / 4]   #op c
+    ])
+    thirds = np.array([
+        [2/3, 1/3, 0],
+        [1/3, 2/3, 0],
+        [0, 2/3, 1/3],
+        [0, 1/3, 2/3],
+        [1/3, 0, 2/3],
+        [2/3, 0, 1/3]
+    ])
+    
+    #drawing triangle
     plt.figure(figsize=(6,6))
     for i in range(3):
-        x = [corners[i][0], corners[(i+1)%3][0]]
-        y = [corners[i][1], corners[(i+1)%3][1]]
-        plt.plot(x, y, 'k-')
+        x1 = [corners[i][0], corners[(i + 1) % 3][0]]
+        y1 = [corners[i][1], corners[(i + 1) % 3][1]]
+        plt.plot(x1, y1, 'k-')
+        x2 = [corners[i][0], middles[i][0]]
+        y2 = [corners[i][1], middles[i][1]]
+        plt.plot(x2, y2, 'k--', linewidth=0.6)
 
-    plt.text(0.5, np.sqrt(3)/2 + 0.05, 'a', ha='center')
-    plt.text(-0.05, -0.05, 'b', ha='right')
-    plt.text(1.05, -0.05, 'c', ha='left')
+    str1 = alt_labels[0] if alt_labels else 'a'
+    str2 = alt_labels[1] if alt_labels else 'b'
+    str3 = alt_labels[2] if alt_labels else 'c'
+    plt.text(-0.05, -0.05, str1, ha='right')
+    plt.text(0.5, np.sqrt(3)/2 + 0.05, str2, ha='center')
+    plt.text(1.05, -0.05, str3, ha='left')
 
-    for i, w in enumerate(w_list):
-        x, y = simplex_to_cartesian(w)
-        label = labels[i] if labels else f'w{i+1}'
+    for i in range(len(thirds)):
+        label = f'P{i+1}'
+        x,y = simplex_to_cartesian(thirds[i])
+        plt.plot(x, y, 'ko')
+        if i == 1 or i == 2:
+            plt.figtext(x, y, label)
+            continue
+        if i == 0:
+            plt.text(x - 0.05, y, label, ha = 'center', va = 'center')
+            continue
+        if i == 3:
+            plt.text(x + 0.05, y, label, ha = 'center', va = 'center')
+            continue
+        plt.text(x, y - 0.05, label, ha = 'center', va = 'top')
+        
+
+    for i in range(len(w_list)):
+        x,y = simplex_to_cartesian(w_list[i])
+        label = w_labels[i] if w_labels else f'w{i+1}'
         plt.plot(x, y, 'o', label=label)
 
     plt.title("Anchor Points in the Simplex")
     plt.axis('equal')
-    plt.axis('off')
+    plt.axis('off') 
     plt.legend()
-    plt.show()
+    #plt.show()
+
+def plot_change_over_alpha(w_list, u, alphas, w_labels=None, alt_labels=None, alt1=0, alt2=1):
+    plt.figure()
+    for i in range(len(w_list)):
+        p_vals = []
+        for alpha in alphas:
+            shifted = apply_anchor(u, w_list[i], alpha)
+            r = [0, 2, 1]
+            p_vals.append(get_prob(shifted, r))
+            label = w_labels[i] if w_labels else f'w{i+1}'
+        plt.plot(alphas, p_vals, label=label)
+
+    plt.xlabel("α (Influence strength)")
+    plt.ylabel("P(a > c)")
+    str1 = alt_labels[alt1] if alt_labels else f'candidate {alt1}'
+    str2 = alt_labels[alt2] if alt_labels else f'candidate {alt2}'
+    plt.title("Effect of Anchor Strength on Support for {} over {}".format(str1, str2))
+    plt.grid(True)
+    plt.tight_layout()
+    plt.xlim(0, 1)   
+    #plt.ylim(0, 1.05)
+    plt.legend()
+    #plt.show()
+
+
+
 
 #testing functions
 
 n = 1000
-u = test_generate_utilities(n)
-w = np.array([0.7, 0.2, 0.1])
-alphas = np.linspace(0, 1, 100)
+u = np.array([
+        [2/3, 1/3, 0],
+        [1/3, 2/3, 0],
+        [0, 2/3, 1/3],
+        [0, 1/3, 2/3],
+        [1/3, 0, 2/3],
+        [2/3, 0, 1/3]
+    ])
+alphas = np.linspace(0.0, 1.0, 100)
 p_vals = []
+w = np.array([[1.0, 0.0, 0.0], [0.5, 0.0, 0.5]])
 
-for alpha in alphas:
-    shifted = apply_anchor(u, w, alpha)
-    p_vals.append(a_over_c_fraction(shifted))
-
-plt.plot(alphas, p_vals)
-plt.xlabel("α (Influence strength)")
-plt.ylabel("P(a > c)")
-plt.title("Probability voter prefers a over c vs. α")
-plt.grid(True)
-plot_simplex_with_anchor([w])
+plot_simplex_with_anchor(w)
+plot_change_over_alpha(w, u, alphas, alt_labels=['a','b','c'])
 plt.show()
